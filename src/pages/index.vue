0ef10dfcc767623f6dca8b79e84a28452081746d
wgy <1476351240@qq.com>
2023-01-23 21:34:44 +0800

    5-2 骨架屏UI补充实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,29 @@
 <template>
   <div>
     <el-row :gutter="20">
+      <template v-if="panels.length == 0">
+        <el-col :span="6" v-for="i in 4" :key="i">
+          <el-skeleton style="width: 100%" animated loading>
+            <template #template>
+              <el-card shadow="hover" class=" border-0">
+                <template #header>
+                  <div class="flex justify-between">
+                    <el-skeleton-item variant="text" style="width: 50%" />
+                    <el-skeleton-item variant="text" style="width: 10%" />
+                  </div>
+                </template>
+                <el-skeleton-item variant="h3" style="width: 80%" />
+                <el-divider />
+                <div class=" flex justify-between text-gray-500">
+                  <el-skeleton-item variant="text" style="width: 50%" />
+                  <el-skeleton-item variant="text" style="width: 10%" />
+                </div>
+              </el-card>
+
+            </template>
+          </el-skeleton>
+        </el-col>
+      </template>
       <!-- elementplus默认一行24列 -->
       <el-col :span="6" :offset="0" v-for="(item, index) in panels" :key="index">
         <el-card shadow="hover" class=" border-0">
